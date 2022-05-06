349e21a4dabf40e3b7d3c94efd28304c839c28ec
Diego Novais <contato@diegonovais.com.br>
2022-05-06 09:53:21 -0300

    starting the function handle

---
+++ lib/servy/handler.ex
@@ -0,0 +1,81 @@
+defmodule Servy.Handler do
+  @moduledoc """
+  The Handler module with the responsibility to make transformation in an request
+  and get the response
+  """
+
+  @doc """
+  Start the function to handle...
+  """
+  def handle(request) do
+    request
+    |> parse()
+    |> route()
+    |> format_response()
+  end
+
+  @doc """
+  Start the parse function doc here
+  """
+  def parse(_request) do
+    # TODO: Parse the request string into map
+    %{method: "GET", path: "/wildthings", response_body: ""}
+  end
+
+  @doc """
+  Start the route function doc here
+  """
+  def route(_conversation) do
+    # TODO: Create a new map that also has the response body:
+    %{method: "GET", path: "/wildthings", response_body: "Bears, Lions, Tigers"}
+  end
+
+  @doc """
+  Start the format_response function doc here
+  """
+  def format_response(_conversation) do
+    # TODO: Use the values in the map to create an HTTP response string
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Bears, Lions, Tigers
+    """
+  end
+end
+
+# HTTP request - Heredoc Elixir (3 double quote in beggining and in the end of block)
+# GET (verb of request)
+# /wildthings (path of request)
+# HTTP/1.1 (the protocol from request)
+# Host (where it request goes to)
+# User-Agent (which browser the request came from)
+# Accept (with wildcard indicates that the request accept any type of request)
+# after a break line
+# An optional body, that we can put the form params from a post request for example
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+# HTTP response - Heredoc Elixir (3 double quote in beggining and in the of block)
+# HTTP/1.1 (The protocol of response)
+# 200 (status code of response)
+# OK (reason of response)
+# Content-Type: (type of response, in this case text/html)
+# Content-Length: (the length of the body in response)
+# after a break line
+# The body of response (content of body)
+# _expected_response = """
+# HTTP/1.1 200 OK
+# Content-Type: text/html
+# Content-Length: 20
+
+# Bears, Lions, Tigers
+# """
+
+IO.inspect(Servy.Handler.handle(request))
